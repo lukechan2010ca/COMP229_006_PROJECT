6fb05a27bc67e5a40d508900ccc0c17928972970
Pak Yin Wong <pakyinwong830@hotmail.com>
2024-11-19 18:09:47 -0500

    sync

--- config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    "ATLASDB":"mongodb+srv://dbuser:pywzOx0Mob7rgWvL@clustercomp229-project.emezu.mongodb.net/project?retryWrites=true&w=majority",
-    "SECRETKEY": "HDGeM}u.;~5k%V$h9@:Qp3"
-}
